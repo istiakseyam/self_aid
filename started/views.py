c2dae6d0822cf4a5089f4facfda8a7d41cd99728
istiak_sheam <istiak.sheam@northsouth.edu>
2023-02-06 10:47:12 +0600

    cse-499 initialized

---
+++ started/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from cgitb import html
+from django.shortcuts import redirect
+
+def home(request):
+    return render(request,'started/includes/home.html')
+
+def record_audio(request):
+    return render(request,'started/includes/record_audio.html')    
